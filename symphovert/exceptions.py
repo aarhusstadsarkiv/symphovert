6298f9412475d2113fdaa56011a8626b529fe08e
Nina Jensen <jnik@aarhus.dk>
2020-03-23 14:39:56 +0100

    Add exceptions.py

---
+++ symphovert/exceptions.py
@@ -0,0 +1,12 @@
+"""Custom exceptions defined for use in convertool modules.
+
+"""
+
+# -----------------------------------------------------------------------------
+# Classes
+# -----------------------------------------------------------------------------
+
+
+class SymphonyError(Exception):
+    """Implements an error to raise when Symphony or related
+    functionality fails."""
