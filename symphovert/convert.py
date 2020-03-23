c540eb96789dd66d7b6fccc688603afef7dcf917
Nina Jensen <jnik@aarhus.dk>
2020-03-23 14:36:51 +0100

    Update convert.py with functionality from convertool

---
+++ symphovert/convert.py
@@ -0,0 +1,128 @@
+"""Tool for converting Lotus SmartSuite files to Open Document format.
+"""
+
+# -----------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------------
+import platform
+import time
+import subprocess
+from subprocess import CalledProcessError
+from pathlib import Path
+import pyperclip
+from convertool.exceptions import SymphonyError
+
+try:
+    import pyautogui
+except Exception as e:
+    raise SymphonyError(f"Failed to import pyautogui with error: {e}")
+
+# -----------------------------------------------------------------------------
+# Globals
+# -----------------------------------------------------------------------------
+pyautogui.PAUSE = 0.4
+pyautogui.FAILSAFE = False
+
+# -----------------------------------------------------------------------------
+# Function Definitions
+# -----------------------------------------------------------------------------
+
+
+def copypaste(str_to_copy: str) -> None:
+    pyperclip.copy(f"{str_to_copy}")
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+
+
+def save_as(file: str) -> None:
+    pyautogui.hotkey("ctrl", "shift", "s", interval=0.1)
+    copypaste(file)
+
+
+def find_symphony() -> str:
+
+    # Initialise variables
+    exe_path: str = ""
+    system: str = platform.system()
+
+    # System specific functionality.
+    if system == "Windows":
+        find_symphony_cmd = (
+            r"cd %programfiles(x86)% && where /r . symphony.exe"
+        )
+    else:
+        raise SymphonyError(
+            f"Conversion using IBM Symphony is not supported on {system}."
+        )
+
+    # Invoke the command to find Symphony on Windows.
+    try:
+        cmd = subprocess.run(
+            f"{find_symphony_cmd}", shell=True, check=True, capture_output=True
+        )
+    except CalledProcessError as error:
+        # Didn't find executable.
+        # Return code != 0
+        error_msg = error.stderr.strip().decode()
+        raise SymphonyError(
+            f"Could not find IBM Symphony with error: {error_msg}"
+        )
+    else:
+        # Remove trailing newline and decode stdout from byte string.
+        # Windows needs the quotes. IBM Symphony has more than one exe file
+        # after install. Finger's crossed the correct one is always the first
+        # Windows finds. :|
+        exe_path = f'"{cmd.stdout.strip().decode().splitlines()[0]}"'
+        return exe_path
+
+
+def symphony_convert(
+    file: Path, outdir: Path, convert_to: str = "odt"
+) -> None:
+    # Initialise variables
+    cmd: str = find_symphony()
+    outfile: Path = outdir.joinpath(f"{file.stem}.{convert_to}")
+
+    # If outfile exists, delete it first.
+    if outfile.is_file():
+        outfile.unlink()
+
+    # Open IBM Symphony and do unholy things with pyautogui
+    try:
+        subprocess.run(cmd, shell=True, check=True, capture_output=True)
+    except CalledProcessError as error:
+        error_msg = error.stderr.strip().decode()
+        raise SymphonyError(
+            f"Execution of IBM Symphony failed with error: {error_msg}"
+        )
+    else:
+        # Wait a bit, then open the file
+        time.sleep(1.5)
+        pyautogui.hotkey("ctrl", "o")
+        copypaste(str(file))
+
+        # Symphony opens an extra menu when ctrl+o is used for... reasons.
+        # Esc closes it.
+        pyautogui.press("escape")
+        time.sleep(2)
+
+        # Save the file as ODT.
+        save_as(str(outfile))
+        time.sleep(2)
+
+        # Kill Symphony
+        # pylint: disable=subprocess-run-check
+        subprocess.run(
+            "taskkill /f /im symphony*", shell=True, capture_output=True
+        )
+        time.sleep(0.1)
+        subprocess.run(
+            "taskkill /f /im soffice*", shell=True, capture_output=True
+        )
+        time.sleep(0.5)
+        # pylint: enable=subprocess-run-check
+
+        # If outfile does not exist after the above, we probably have a
+        # problem.
+        if not outfile.is_file():
+            raise SymphonyError(f"Conversion of {file} failed!")
